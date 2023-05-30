7dd73825e228ecd32afb7baed39fdbce1b34f553
AbrarNitk <abrar.nitk@gmail.com>
2023-05-30 22:40:05 +0530

    added github login fn

--- service/auth/src/controller.rs
+++ service/auth/src/controller.rs
@@ -5,10 +5,16 @@ pub async fn routes(
     let query = url::form_urlencoded::parse(req.uri().query().unwrap_or("").as_bytes())
         .into_owned()
         .collect::<std::collections::HashMap<String, String>>();
-    if !query.contains_key("platform") {
-        let bytes = tokio::fs::read("service/auth/login.html").await?;
-        Ok(hyper::Response::new(hyper::Body::from(bytes)))
-    } else {
-        Ok(hyper::Response::new(hyper::Body::empty()))
+    match query.get("platform") {
+        Some(p) if p.eq("github") => Ok(crate::github::login(&req).await.unwrap()),
+        Some(p) if p.eq("gitlab") => Ok(hyper::Response::new(hyper::Body::empty())),
+        Some(p) if p.eq("google") => Ok(hyper::Response::new(hyper::Body::empty())),
+        Some(p) if p.eq("twitter") => Ok(hyper::Response::new(hyper::Body::empty())),
+        Some(p) if p.eq("discord") => Ok(hyper::Response::new(hyper::Body::empty())),
+        Some(p) if p.eq("linkedin") => Ok(hyper::Response::new(hyper::Body::empty())),
+        _ => {
+            let bytes = tokio::fs::read("service/auth/login.html").await?;
+            Ok(hyper::Response::new(hyper::Body::from(bytes)))
+        }
     }
 }
