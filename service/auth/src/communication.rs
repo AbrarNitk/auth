b4cd475d134b4440f154097606358500767ca08a
AbrarNitk <abrar.nitk@gmail.com>
2023-08-22 22:03:57 +0530

    send email to the client

--- service/auth/src/communication.rs
+++ service/auth/src/communication.rs
@@ -1,8 +1,5 @@
 static EMAIL_TEMPLATE: &str = include_str!("email-template.html");
 
-#[derive(thiserror::Error, Debug)]
-pub enum SendMailError {}
-
 #[derive(serde::Serialize)]
 pub struct EmailUser {
     #[serde(rename = "name")]
@@ -31,6 +28,16 @@ pub struct SendEmailReq {
     pub tags: Vec<String>,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum SendMailError {
+    #[error("InvalidHeaderValueError: {}", _0)]
+    InvalidHeaderValue(#[from] hyper::header::InvalidHeaderValue),
+    #[error("ReqwestError: {}", _0)]
+    Reqwest(#[from] reqwest::Error),
+    #[error("SerdeSerializeError: {}", _0)]
+    Serde(#[from] serde_json::Error),
+}
+
 pub async fn send_email() -> Result<(), SendMailError> {
     let mut headers = hyper::HeaderMap::new();
     headers.insert(
@@ -38,8 +45,45 @@ pub async fn send_email() -> Result<(), SendMailError> {
         hyper::header::HeaderValue::from_static("application/json"),
     );
     headers.insert(
-        hyper::header::CONTENT_TYPE,
-        hyper::header::HeaderValue::try_from("application/json").unwrap(),
+        "api-key",
+        hyper::header::HeaderValue::try_from(crate::BREVO_API_KEY.as_str())?,
     );
+
+    let request = SendEmailReq {
+        sender: EmailUser {
+            email: "wilderbit.net@gmail.com".to_owned(),
+            name: "Wilderbit".to_owned(),
+        },
+        to: vec![EmailUser {
+            email: "manishmsiclub@gmail.com".to_owned(),
+            name: "Manish Jain".to_owned(),
+        }],
+        bcc: vec![],
+        cc: vec![EmailUser {
+            email: "wilderbit.net@gmail.com".to_owned(),
+            name: "Wilderbit".to_owned(),
+        }],
+        html_content: EMAIL_TEMPLATE.to_string(),
+        subject: "🔒 [Hasinam]: Your One-Time Password (OTP) for Secure Access".to_string(),
+        reply_to: None,
+        tags: vec!["OTP".to_owned()],
+    };
+
+    let client = reqwest::Client::new();
+    let payload = serde_json::to_string(&request)?;
+    let response = client
+        .post("")
+        .headers(headers)
+        .json(&payload)
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let body = response.text().await?;
+        println!("Send Email Response Success Body: {}", body);
+    } else {
+        println!("Send Email Error: {}", response.status());
+    }
+
     Ok(())
 }
--- service/auth/src/lib.rs
+++ service/auth/src/lib.rs
@@ -3,3 +3,10 @@ pub mod controller;
 pub mod error;
 mod github;
 pub mod utils;
+
+pub(crate) static BREVO_API_KEY: once_cell::sync::Lazy<String> = {
+    once_cell::sync::Lazy::new(|| match std::env::var("BREVO_API_KEY") {
+        Ok(val) => val,
+        Err(e) => panic!("{}{}", "BREVO_API_KEY not found in env ", e),
+    })
+};
