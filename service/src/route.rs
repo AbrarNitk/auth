31abfec1611fddeeb022e5ad79e570a6905d0d12
AbrarNitk <abrar.nitk@gmail.com>
2023-09-19 15:48:05 +0530

    added health api

--- service/src/route.rs
+++ service/src/route.rs
@@ -1,8 +1,21 @@
+
+
 pub async fn handler(
     req: hyper::Request<hyper::Body>,
     db_pool: db::pg::DbPool,
 ) -> Result<hyper::Response<hyper::Body>, http_service::errors::RouteError> {
     println!("{}:{}", req.method(), req.uri().path());
+
+    if req.uri().path().starts_with("/health") {
+        let mut response = hyper::Response::new(hyper::Body::empty());
+        *response.body_mut() = hyper::Body::from(serde_json::json!({"success": true}).to_string());
+        *response.status_mut() = hyper::StatusCode::OK;
+        response.headers_mut().append(
+            hyper::header::CONTENT_TYPE,
+            hyper::http::HeaderValue::from_str("application/json").unwrap(), // TODO: Remove unwrap
+        );
+        return Ok(response);
+    }
 
     if req.uri().path().starts_with("/auth/") || req.uri().path().starts_with("/api/auth/") {
         return Ok(auth::controller::routes(req, db_pool).await?);
